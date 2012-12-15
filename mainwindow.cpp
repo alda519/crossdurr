6293989b5ec45b0eb23f694d7b83c2a22c755a15
michal <michal@VirtualUbuntu.(none)>
2012-12-15 01:31:20 +0100

    rozdilny toolbary pro vytvareni a lusteni

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include "questfield.h"
 #include "editfield.h"
 #include <QString>
+#include <QtCore>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,49 +14,162 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->actionKonec,SIGNAL(activated()), this, SLOT(close()));
     connect(ui->actionOtev_t, SIGNAL(activated()), this, SLOT(otevriKrizovku()));
 
-    //Field * policka[5][5];
-
     krizovkaWidth = 0;
     krizovkaHeight = 0;
 
     setMinimumSize(40 + 160 + 5 * 52, 250 + 5 * 52);
     setMaximumSize(40 + 160 + 5 * 52, 250 + 5 * 52);
 
-    // leve menu
-    QVBoxLayout * wiwi = new QVBoxLayout;
-      // nastaveni velikosti
-    wiwi->addWidget(new QLabel("Sirka"));
-    kSirka = new QLineEdit("15");
-    kVyska = new QLineEdit("10");
-    wiwi->addWidget(kSirka);
-    wiwi->addWidget(new QLabel("Vyska"));
-    wiwi->addWidget(kVyska);
-    QPushButton * newKrizovkaBtn = new QPushButton(QString("Nastavit velikost"));
-    wiwi->addWidget(newKrizovkaBtn);
-      // tajenka
-    wiwi->addWidget(new QLabel("Tajenka"));
-    tAjenka = new QTextEdit("DESNE SUPER TAJENKA");
-    tAjenka->setMaximumWidth(160);
-    tAjenka->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::MinimumExpanding);
-    wiwi->addWidget(tAjenka);
-    wiwi->addWidget(new QPushButton(QString("Umistit tajenku")));
-        // tooly
-    wiwi->addWidget(new QRadioButton("AAAA"));
-    wiwi->addWidget(new QRadioButton("BBBB"));
-    wiwi->addWidget(new QRadioButton("CCCC"));
-      // cervene tlacitko
-    QPushButton * finishBtn = new QPushButton("Dokocit krizovku");
-    finishBtn->setStyleSheet("background-color: #ff3333;");
-    wiwi->addWidget(finishBtn);
-    ui->widget->setLayout(wiwi);
+    vytvorLusticiListu();
+    vytvorVytvareciListu();
 
-    connect(newKrizovkaBtn, SIGNAL(pressed()), this, SLOT(novaKrizovka()));
-    connect(finishBtn, SIGNAL(pressed()), this, SLOT(vyplnitNesmyslama()));
+    ui->widgetVytvareni->setLayout(listaVytvareni);
+    ui->widgetLusteni->setLayout(listaLusteni);
+
+    zobrazVytvareciListu();
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+/*
+ * menu vlevo naboku, zobrazuje se jenom pri vytvareni krizovky
+ */
+void MainWindow::vytvorVytvareciListu()
+{
+
+    listaVytvareni = new QVBoxLayout;
+      // nastaveni velikosti
+    listaVytvareni->addWidget(new QLabel("Sirka"));
+    kSirka = new QLineEdit("15");
+    kVyska = new QLineEdit("10");
+    listaVytvareni->addWidget(kSirka);
+    listaVytvareni->addWidget(new QLabel("Vyska"));
+    listaVytvareni->addWidget(kVyska);
+    QPushButton * newKrizovkaBtn = new QPushButton(QString("Nastavit velikost"));
+    listaVytvareni->addWidget(newKrizovkaBtn);
+      // tajenka
+    listaVytvareni->addWidget(new QLabel("Tajenka"));
+    tAjenka = new QTextEdit("DESNE SUPER TAJENKA");
+    tAjenka->setMaximumWidth(160);
+    tAjenka->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::MinimumExpanding);
+    listaVytvareni->addWidget(tAjenka);
+    listaVytvareni->addWidget(new QPushButton(QString("Umistit tajenku")));
+        // tooly
+//    listaVytvareni->addWidget(new QRadioButton("AAAA"));
+//    listaVytvareni->addWidget(new QRadioButton("BBBB"));
+//    listaVytvareni->addWidget(new QRadioButton("CCCC"));
+
+
+     createActions();
+     listaVytvareni->addWidget(tools);
+
+
+      // cervene tlacitko
+    QPushButton * finishBtn = new QPushButton("Doplnit prazdne");
+    finishBtn->setStyleSheet("background-color: #ff3333;");
+    listaVytvareni->addWidget(finishBtn);
+
+    QPushButton * lustitBtn = new QPushButton("Lustit");
+    lustitBtn->setStyleSheet("background-color: #ff3333;");
+    listaVytvareni->addWidget(lustitBtn);
+
+    connect(newKrizovkaBtn, SIGNAL(pressed()), this, SLOT(novaKrizovka()));
+    connect(finishBtn, SIGNAL(pressed()), this, SLOT(vyplnitNesmyslama()));
+    connect(lustitBtn, SIGNAL(pressed()), this, SLOT(zobrazLusticiListu()));
+
+}
+
+/*
+ * menu vlevo naboku, zobrazuje se jenom pri lusteni
+ */
+void MainWindow::vytvorLusticiListu()
+{
+    listaLusteni = new QVBoxLayout;
+      // nastaveni velikosti
+
+      // tajenka
+    listaLusteni->addWidget(new QLabel("Tajenka"));
+    tAjenka = new QTextEdit("DESNE SUPER TAJENKA");
+    tAjenka->setMaximumWidth(160);
+    tAjenka->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::MinimumExpanding);
+    listaLusteni->addWidget(tAjenka);
+    listaLusteni->addWidget(new QPushButton(QString("Umistit tajenku")));
+
+    QPushButton * editorBtn = new QPushButton("Zpet do editoru");
+    editorBtn->setStyleSheet("background-color: #ff3333;");
+    listaLusteni->addWidget(editorBtn);
+    connect(editorBtn, SIGNAL(pressed()), this, SLOT(zobrazVytvareciListu()));
+
+}
+
+void MainWindow::createActions()
+{
+    actEdit = new QAction("pismeno",this);
+    actEdit->setStatusTip("Prazdne policko pro 1 pismeno");
+    connect(actEdit,SIGNAL(triggered()),this,SLOT(selectEdit()));
+
+    actTajenka = new QAction("tajenka",this);
+    actTajenka->setStatusTip("Prazdne policko pro 1 pismeno tajenky");
+    connect(actTajenka,SIGNAL(triggered()),this,SLOT(selectTajenka()));
+
+    actSymbol = new QAction("symbol",this);
+    actSymbol->setStatusTip("Pro vyplneni rohu");
+    connect(actSymbol,SIGNAL(triggered()),this,SLOT(selectSymbol()));
+
+    actQuest = new QAction("zadani",this);
+    actQuest->setStatusTip("Policko se zadanim pro 1 slovo");
+    connect(actQuest,SIGNAL(triggered()),this,SLOT(selectQuest()));
+
+    actTools = new QActionGroup(this);
+    actTools->addAction(actEdit);
+    actTools->addAction(actTajenka);
+    actTools->addAction(actSymbol);
+    actTools->addAction(actQuest);
+
+    tools = new QToolBar();
+    tools->addAction(actEdit);
+    tools->addAction(actTajenka);
+    tools->addAction(actSymbol);
+    tools->addAction(actQuest);
+
+}
+
+void MainWindow::zobrazVytvareciListu()
+{
+    ui->widgetVytvareni->setVisible(true);
+    ui->widgetLusteni->setVisible(false);
+}
+
+void MainWindow::zobrazLusticiListu()
+{
+    ui->widgetVytvareni->setVisible(false);
+    ui->widgetLusteni->setVisible(true);
+
+}
+
+void MainWindow::selectEdit()
+{
+    qDebug() << "edit" << endl;
+}
+
+void MainWindow::selectTajenka()
+{
+    qDebug() << "tajenka" << endl;
+}
+
+void MainWindow::selectSymbol()
+{
+    qDebug() << "symbol" << endl;
+}
+
+void MainWindow::selectQuest()
+{
+    qDebug() << "quest" << endl;
 }
 
 void MainWindow::otevriKrizovku()
--- mainwindow.h
+++ mainwindow.h
@@ -17,16 +17,41 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     QVector< QVector<Field *> > * policka;
     ~MainWindow();
+
+    QVBoxLayout *listaVytvareni;            // bocni lista vlevo co se zobrazuje pri vytvareni krizvoky
+    QVBoxLayout *listaLusteni;              // bocni lista vlevo co se zobrazuje pri lusteni krizovky
+
+    void vytvorVytvareciListu();
+    void vytvorLusticiListu();
     
 private:
     Ui::MainWindow *ui;
     int krizovkaWidth, krizovkaHeight;
     QLineEdit * kSirka, * kVyska;
     QTextEdit * tAjenka;
+
+    // tlacitka s modama
+    QToolBar * tools;
+    QActionGroup *actTools;
+    QAction *actEdit;
+    QAction *actTajenka;
+    QAction *actSymbol;
+    QAction *actQuest;
+    void createActions();
+
+
 public slots:
     void otevriKrizovku();
     void novaKrizovka();
     void vyplnitNesmyslama();
+    void zobrazVytvareciListu();
+    void zobrazLusticiListu();
+
+    void selectEdit();
+    void selectTajenka();
+    void selectSymbol();
+    void selectQuest();
+
 };
 
 #endif // MAINWINDOW_H
